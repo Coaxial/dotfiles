79ca05a280c58a7d73a7ee00ac2ec85eb8198585
coaxial <hi@64b.it>
2020-11-21 21:02:27 +0100

    Add ssh and git tasks

--- ansible-playbook/molecule/default/tests/test_default.py
+++ ansible-playbook/molecule/default/tests/test_default.py
@@ -1,3 +1,5 @@
+import re
+
 """Role testing files using testinfra."""
 
 
@@ -12,3 +14,24 @@ def test_ssh_keypair(host):
         assert key.user == "root"
         assert key.group == "root"
         assert key.mode == 0o600
+
+        assert keys[1].contains("test@example.org")
+
+def test_git(host):
+    git = host.package("git")
+
+    assert git.is_installed
+
+def test_git_local_config(host):
+    gitconfig=host.file("/root/.gitconfig_local")
+
+    assert gitconfig.contains("test@example.org")
+    assert gitconfig.user == "root"
+    assert gitconfig.group == "root"
+    assert gitconfig.mode == 0o600
+
+def test_dotfiles_repo_config(host):
+    repo_url = host.run("cd /root/dotfiles && git config --get remote.origin.url")
+    pattern = re.compile("^git@.+:[-\w]+/[-\w]+\.git$")
+
+    assert pattern.match(repo_url.stdout)
