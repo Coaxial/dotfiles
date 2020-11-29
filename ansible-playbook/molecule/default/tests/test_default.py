fddefc0c9b8540915a347bf067afbc4647169152
coaxial <hi@64b.it>
2020-11-29 20:09:50 +0100

    wip backups

--- ansible-playbook/molecule/default/tests/test_default.py
+++ ansible-playbook/molecule/default/tests/test_default.py
@@ -4,17 +4,23 @@ import re
 
 def test_ssh_keypair(host):
     keys = [
-        host.file("/root/.ssh/id_ed25519"),
-        host.file("/root/.ssh/id_ed25519.pub")
+        host.file("/home/ansible/.ssh/id_ed25519"),
+        host.file("/home/ansible/.ssh/id_ed25519.pub")
     ]
+    dir = host.file("/home/ansible/.ssh")
 
     for key in keys:
         assert key.exists
-        assert key.user == "root"
-        assert key.group == "root"
+        assert key.user == "ansible"
+        assert key.group == "ansible"
         assert key.mode == 0o600
 
         assert keys[1].contains("test@example.org")
+
+    assert dir.exists
+    assert dir.user == "ansible"
+    assert dir.group == "ansible"
+    assert dir.mode == 0o700
 
 def test_git(host):
     git = host.package("git")
@@ -22,15 +28,15 @@ def test_git(host):
     assert git.is_installed
 
 def test_git_local_config(host):
-    gitconfig=host.file("/root/.gitconfig_local")
+    gitconfig=host.file("/home/ansible/.gitconfig_local")
 
     assert gitconfig.contains("test@example.org")
-    assert gitconfig.user == "root"
-    assert gitconfig.group == "root"
+    assert gitconfig.user == "ansible"
+    assert gitconfig.group == "ansible"
     assert gitconfig.mode == 0o600
 
 def test_dotfiles_repo_config(host):
-    repo_url = host.run("cd /root/dotfiles && git config --get remote.origin.url")
+    repo_url = host.run("cd /home/ansible/dotfiles && git config --get remote.origin.url")
     pattern = re.compile(r'^git@.+:[-\w]+/[-\w]+\.git$')
 
     assert pattern.match(repo_url.stdout)
@@ -71,8 +77,8 @@ def test_dotfiles(host):
     ]
 
     for filename in filenames:
-        dotfile = host.file("/root/" + filename)
-        assert dotfile.linked_to == "/root/dotfiles/ansible-playbook/files/home/" + filename
+        dotfile = host.file("/home/ansible/" + filename)
+        assert dotfile.linked_to == "/home/ansible/dotfiles/ansible-playbook/files/home/" + filename
 
 def test_neovim(host):
     neovim = host.package('neovim')
@@ -80,7 +86,7 @@ def test_neovim(host):
     assert neovim.is_installed
 
 def test_neovim_plugins(host):
-    plugins_dir = host.file('/root/.vim/autoload')
+    plugins_dir = host.file('/home/ansible/.vim/autoload')
 
     assert plugins_dir.exists
 
@@ -99,3 +105,29 @@ def test_nvm_install(host):
     nvm_version_pattern = re.compile(r'^\d+\.\d+\.\d+')
 
     assert nvm_version_pattern.match(nvm_version)
+
+def test_backups(host):
+    packages = ['borgbackup', 'borgmatic']
+    config_files = ['borgmatic.service', 'borgmatic.timer']
+    config_dirs = ['/home/ansible/.config/systemd', '/home/ansible/.config/systemd/user']
+
+    assert host.service('borgmatic').is_enabled
+    assert host.service('borgmatic').is_running
+
+    for package in packages:
+        assert host.package(package).is_installed
+
+    for config_file in config_files:
+        file = host.file(config_file)
+
+        assert file.exists
+        assert file.user == 'ansible'
+        assert file.group == 'ansible'
+        assert file.mode == 0o600
+
+    for config_dir in config_dirs:
+        d = host.file(config_dir)
+
+        assert d.user == 'ansible'
+        assert d.group == 'ansible'
+        assert d.mode == 0o700
